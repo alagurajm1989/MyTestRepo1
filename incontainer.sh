5cadf81f42bcc57d37467b2ab7bb67c703a1d9d4
alagurajm1989 <alaguraj.mathialagan@onmobile.com>
2022-12-14 13:22:14 +0530

    added

--- incontainer.sh
+++ incontainer.sh
@@ -1,5 +1,6 @@
+#!/bin/sh
 docker pull ubuntu
 docker images
-mkdir ca-test && cd ca-test
+mkdir /opt/ca-test && cd /opt/ca-test
 echo -e "FROM ubuntu:18.04 \nLABEL maintainer='Alaguraj <alaguraj.vkm@gmail.com>'\nRUN apt-get update && \\ \n    apt-get -qy full-upgrade && \\ \n    apt-get install -qy curl && \\ \n    apt-get install -qy curl && \\ \n    curl -sSL https://get.docker.com/ | sh" > Dockerfile
 docker build -t test-image .
